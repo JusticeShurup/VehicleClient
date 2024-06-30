095d83936ba78eb3062ed00558f83bb283b6d0e7
Grishin Michael <69842449+JusticeShurup@users.noreply.github.com>
2024-06-30 23:43:00 +0500

    Initial

---
+++ VehicleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using ProtosContract;
+
+namespace VehicleClient
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            using var channelParking = GrpcChannel.ForAddress("https://localhost:7271");
+            var client = new Vehicles.VehiclesClient(channelParking);
+            var result = client.DeleteVehicle(new DeleteVehicleRq() { Id = "d990a043-45fa-4ea5-b84a-9c62d637461e" });
+
+            Console.Write(result.Success);
+            Console.ReadLine();
+        }
+    }
+}
